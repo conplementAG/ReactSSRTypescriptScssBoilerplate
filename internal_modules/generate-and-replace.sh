84dee057a37befeb20357510561b432a223d2ba4
Denis Biondic <Denis.Biondic@conplement.de>
2018-10-29 17:27:44 +0100

    wip: swagger code gen

--- internal_modules/generate-and-replace.sh
+++ internal_modules/generate-and-replace.sh
@@ -4,15 +4,6 @@ set -eo pipefail
 
 java -jar /opt/swagger-codegen-cli/swagger-codegen-cli.jar generate \
   -i https://petstore.swagger.io/v2/swagger.json \
-  -l typescript-fetch \
-  -o /internal_modules/petstore-bff-proxy
- 
- # --additional-properties usePromises=true,useES6=false
-
-# echo 'HACK : :) replacing the string replace from generated proxy'
-
-# sedeasy () {
-#   sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
-# }
-
-# sedeasy "str.replace(/T/i, ' ')" "str" /internal_modules/pex-bff-proxy/src/ApiClient.js
+  -l javascript \
+  -o /internal_modules/petstore-bff-proxy \
+  --additional-properties usePromises=true,useES6=false,projectName=petstore-bff-proxy
--- razzle.config.js
+++ razzle.config.js
@@ -2,4 +2,18 @@
 
 module.exports = {
   plugins: ['typescript'],
+
+  modify(config) {
+    const appConfig = config; // stay immutable here
+
+    // AMD should be turned off by default, since some libraries (like swagger codegen)
+    // generate node libraries that have issues with AMD
+    appConfig.module.rules.push({
+      parser: {
+        amd: false
+      }
+    });
+
+    return appConfig;
+  },
 };
